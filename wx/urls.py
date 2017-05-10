c268b088be87b7a1c3b561d2b10e87de0747e806
end-e <xiuliang555@foxmail.com>
2017-05-10 18:27:44 +0800

    # xiul # # 新增微信文件校验url

--- wx/urls.py
+++ wx/urls.py
@@ -2,14 +2,17 @@
 from django.conf.urls import include, url
 import xadmin
 from xadmin.plugins import xversion
+
+from apps.api import views
+
 xadmin.autodiscover()
 
 xversion.register_models()
 
 urlpatterns = [
     url(r'xadmin/', include(xadmin.site.urls)),
-    # 微信接入url (www.zisai.net)
-    url(r'^$', 'api.views.conn', name='check_signature'),
+    # 微信文件校验地址
+    url(r'MP_verify_QthEcNlYA73MNXgH.txt', views.verify, name='MP_verify_QthEcNlYA73MNXgH.txt'),
 
     # api
     url(r'^api/', include('api.urls', namespace='api')),
