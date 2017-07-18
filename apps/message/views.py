2de5a34e3b6884b3b95ec9c8132f1e431c9d8a45
end-e <xiuliang555@foxmail.com>
2017-07-18 16:49:54 +0800

    # xiul # # 新增图片链接地址

--- apps/message/views.py
+++ apps/message/views.py
@@ -1,36 +1,140 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+import time
+
+from django.db import Error
+from django.core.exceptions import ObjectDoesNotExist
+from user.models import WechatMembers
 
 from wechatpy.replies import TextReply, create_reply
+from utils import db
 
 
 def switch_type(msg):
     """
-    推送消息公共属性
-    id 消息id，64位整数
-    source 消息的来源用户，即发送消息的用户
-    target 消息的目标用户
-    create_time 消息的发送时间，UNIX时间戳
-    type 消息的类型
-
-    推送文本消息属性（用户发送给服务号消息）
-    type: text
-    content: 消息的内容
+    微信推送事件分发
     """
-    from_id = msg.id
-    from_source = msg.source
-    from_target = msg.target
-    from_create_time = msg.create_time
     from_type = msg.type
-    from_content = msg.content
 
+    # 推送文字消息
     if from_type == 'text':
-        if from_content == '红包':
+        if msg.content == '红包':
             to_content = '活动已结束！'
+        elif msg.content == '解除绑定':
+            try:
+                WechatMembers.objects.filter(openid=msg.source).delete()
+                to_content = '解除绑定成功。'
+            except Error:
+                to_content = '嗨，小宽发现您还未绑定会员。\n' \
+                             '<a href="https://open.weixin.qq.com/connect/oauth2/authorize?' \
+                             'appid=wx5afe243d26d9fe30&redirect_uri=http%3A//www.zisai.net/user/' \
+                             'membersbound&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect">' \
+                             '点击这里</a>绑定会员'
         else:
-            to_content = '本公众号主要提供会员绑定、消费提醒等，如需查看门店营业时间和电话、海报信息、各种活动推广等请关注公众号“宽广超市”，感谢关注。'
+            to_content = '本公众号主要提供会员绑定、消费提醒等，如需查看门店营业时间和电话、海报信息、' \
+                         '各种活动推广等请关注公众号“宽广超市”，感谢关注。'
         reply = TextReply(content=to_content, message=msg)
         xml = reply.render()
         return xml
+    # 推送事件
+    elif from_type == 'event':
+        # 关注事件
+        if msg.event == 'subscribe':
+            to_content = '嗨，欢迎关注，我是小宽。宽广超市全新礼品卡上线了，送朋友传心意，好玩有趣，还能发图片录视频嘞。' \
+                         '点击菜单\"礼品卡\"传心意吧~'
+            reply = TextReply(content=to_content, message=msg)
+            xml = reply.render()
+            return xml
+        # 自定义菜单事件
+        elif msg.event == 'click':
+            # 解除绑定
+            if msg.key == 'jcbd':
+                try:
+                    WechatMembers.objects.get(openid=msg.source)
+                    to_content = '发送\"解除绑定\"关键字，解除会员绑定。解除绑定后将无法使用会员相关功能。'
+                except ObjectDoesNotExist:
+                    to_content = '嗨，我是小宽，您还没有操作会员绑定呢。'
+                reply = TextReply(content=to_content, message=msg)
+                xml = reply.render()
+                return xml
+            # 消费记录
+            elif msg.key == 'xfjl':
+                # 通过openid 查询用户是否绑定过微信
+                user_set = WechatMembers.objects.filter(openid=msg.source)
+                # 已绑定
+                if user_set:
+
+                    # 会员卡号
+                    member_number = ''
+                    for user in user_set:
+                        member_number = user.membernumber
+
+                    # 卡服 获取消费明细
+                    conn = db.getMsSqlConn()
+                    cursor = conn.cursor()
+
+                    # 查询最后一次消费的明细
+                    last_buy_sql = "SELECT ListNo,SaleValue,DiscValue,SDate FROM CardSaleGoods " \
+                                   "WHERE cardno='{member_number}' " \
+                                   "AND SDate IN " \
+                                   "(SELECT CONVERT(CHAR(10),lastusedate,120) " \
+                                   "FROM Guest " \
+                                   "WHERE cardno='{member_number}')".format(member_number=member_number)
+                    cursor.execute(last_buy_sql)
+                    last_buy_rs = cursor.fetchall()
+                    cursor.close()
+                    conn.close()
+
+                    # 如果没有查询到消费明细 再取Guest表的数据
+                    if not last_buy_rs:
+                        conn = db.getMsSqlConn()
+                        cursor = conn.cursor()
+
+                        # 查询今天是否消费
+                        today_buy_sql = "SELECT LastUseDate FROM Guest WHERE cardno='{member_number}'".format(
+                            member_number=member_number)
+                        cursor.execute(today_buy_sql)
+                        today_buy_rs = cursor.fetchall()
+                        cursor.close()
+                        conn.close()
+
+                        # 格式化时间
+                        today = '{0:%Y-%m-%d}'.format(today_buy_rs[0]['LastUseDate'])
+                        now = str(time.strftime("%Y-%m-%d", time.localtime()))
+
+                        # 判断今天是否消费
+                        if today == now:
+                            to_content = '客官，数据正在努力奔跑中...请以今天超市打印的购物小票为准，或者明天再来看看吧'
+                        else:
+                            to_content = '客官最近都没来购物，小宽好想你呦~'
+                    # 如果查询到消费明细
+                    else:
+                        # 存放当天消费明细 如果有多次消费 存放格式[{...}, {...}]
+                        temp_list = []
+
+                        # 合并同一小票号数据
+                        for row in last_buy_rs:
+                            if not temp_list:
+                                temp_list.append(row)
+                            else:
+                                for d in temp_list:
+                                    if row['ListNo'] == d['ListNo']:
+                                        d['SaleValue'] += row['SaleValue']
+                                        d['DiscValue'] += row['DiscValue']
+                                    else:
+                                        temp_list.append(row)
+
+                        # 消费次数
+                        records = len(temp_list)
+                        # 消费日期
+                        date = '{0:%Y-%m-%d}'.format(temp_list[0]['SDate'])
+
+                        to_content = '日期: {sdate}\n消费次数: {records}次\n'.format(sdate=date, records=records)
+                # 未绑定
+                else:
+                    to_content = '嗨，我是小宽，你需要操作会员绑定，我才能帮你查询呐，如你已经是实名制会员，请点击会员绑定。'
+
+                reply = TextReply(content=to_content, message=msg)
+                xml = reply.render()
+                return xml
     else:
         return 'success'
