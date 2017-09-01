a8ce754f84fb300521562fe9663ffbf105c6e5b0
end-e <xiuliang555@foxmail.com>
2017-09-01 15:04:49 +0800

    # xiul # # 新增爱宽广服务号获取access_token

--- apps/admin/utils/myClass.py
+++ apps/admin/utils/myClass.py
@@ -1,25 +1,33 @@
 # -*-  coding:utf-8 -*-
-__author__ = ''
-__date__ = '2017/6/15 10:06'
+# __author__ = ''
+# __date__ = '2017/6/15 10:06'
 from django.views.generic.base import View
 from django.core.cache import caches
 
-from utils import wx,consts
+from utils import wx, consts
+
 
 class MyView(View):
-    def __init__(self,**kwargs):
-        super(MyView,self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super(MyView, self).__init__(**kwargs)
         access_token = caches['default'].get('wx_kgcs_access_token', '')
         if not access_token:
             wx.get_access_token('kgcs', consts.KG_APPID, consts.KG_APPSECRET)
         self.token = access_token
 
 
+class MyViewIkg(View):
+    def __init__(self, **kwargs):
+        super(MyViewIkg, self).__init__(**kwargs)
+        access_token = caches['default'].get('wx_ikg_access_token', '')
+        if not access_token:
+            wx.get_access_token('ikg', consts.APPID, consts.APPSECRET)
+        self.token = access_token
+
+
 class MyException(Exception):
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return repr(self.value)
-
-
-
